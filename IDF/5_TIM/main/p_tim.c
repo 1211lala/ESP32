dcd71baad34f9a8975fe1abbef644e2b52712a98
liuao <2494210546@qq.com>
2024-02-21 15:32:42 +0800

    commit

--- IDF/5_TIM/main/p_tim.c
+++ IDF/5_TIM/main/p_tim.c
@@ -1,6 +1,38 @@
 /*
+
+https://blog.csdn.net/m0_50064262/article/details/115407884
+
+
+## ESP32定时器资源
     ESP32 内置4 个64-bit 通用定时器,分为两组，每组两个
     每个定时器包含一个16-bit 预分频器和一个64-bit 可自动重新加载向上／向下计数器。
+
+##  回调配置法(好用)
+　　ESP32 硬件定时器在默认情况下有一个 isr 程序，称作 “总 ISR 程序” 。它为我们执行我们配置的定时器的回调。
+    我们写的回调函数，配置后将属于这个ISR程序的一部分。因此这个回调函数又称为ISR 回调（ISR callback）
+        调用函数timer_isr_callback_add()添加回调
+        调用函数timer_isr_callback_remove()移除回调
+
+
+## 自定义配置isr
+    如果使用此函数重新注册 ISR，则需要编写完整的 ISR。例如需要清除中断标志位，配置自旋锁等。
+        调用 timer_isr_register(0, 0, timerIsr, &config, ESP_INTR_FLAG_IRAM, NULL) 注册中断函数
+        示例中断函数
+
+        void IRAM_ATTR timerIsr(void *arg)
+        {
+            // 获取自旋锁
+            timer_spinlock_take(0); 
+
+            //代码区1
+            // 清除中断标志位
+            timer_group_clr_intr_status_in_isr(0, 0);
+            // 重新使能alarm事件
+            timer_group_enable_alarm_in_isr(0, 0);
+
+            //代码区2
+            timer_spinlock_give(0);
+        }
 */
 
 #include "p_tim.h"
